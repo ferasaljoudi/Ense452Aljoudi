6ae11c7a36342c9de5430660c6b81aeadf71f67e
Feras Aljoudi <feras.aljoudi@gmail.com>
2024-09-22 17:18:03 -0600

    Update Phase2 in main.c

--- Lab1/Core/Src/main.c
+++ Lab1/Core/Src/main.c
@@ -105,6 +105,7 @@ int main(void)
 	  HAL_Delay(1000);
 	  */
 
+	  /* Phase 2 */
 	  if (__HAL_TIM_GET_COUNTER(&htim4) >= 1000) {
 		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 		  __HAL_TIM_SET_COUNTER(&htim4, 0);
